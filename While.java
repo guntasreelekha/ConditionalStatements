7a0bdd1bebf4192d57494a92b71dd57fd2d6c110
guntasreelekha <guntasreelekha@gmail.com>
2020-09-09 21:40:14 +0530

    Conditional Statements

---
+++ While.java
@@ -0,0 +1,11 @@
+package conditionalstatements;
+
+public class While {
+    public static void main(String args[]) {
+        int n = 5;
+        while (n > 0) {
+            System.out.println("Tick " + n);
+            n--;
+        }
+    }
+}
